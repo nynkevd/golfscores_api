d2ee7b237279e1fe76e8fbd9680bea247d7a5e47
nynkevd <37672196+nynkevd@users.noreply.github.com>
2020-02-25 15:29:35 +0100

    Adding admins to a group

--- controllers/group-controller.js
+++ controllers/group-controller.js
@@ -92,6 +92,51 @@ const addPlayerToGroup = async (req, res, next) => {
     res.status(201).json({message: "Success"});
 };
 
+const addAdminToGroup = async (req, res, next) => {
+    validateRequest(req, next);
+
+    const {newAdmins, group, user} = req.body;
+
+    let existingGroup;
+    try {
+        existingGroup = await Group.findById(group);
+    } catch (e) {
+        return next(new HttpError("Could not add new player(s), please try again", 500))
+    }
+    if (!existingGroup) {
+        return next(new HttpError("The provided group could not be found", 404))
+    }
+
+    // CHECK IF USER IS ADMIN ON GROUP
+    const admins = existingGroup['admins'];
+    if (!admins.includes(user)) {
+        return next(new HttpError("The provided user cannot update the provided group", 405))
+    }
+
+    // CHECK IF PROVIDED ADMINS ARE IN GROUP
+    const existingPlayers = existingGroup['players'];
+    const existingAdmins = existingGroup['admins'];
+    let adminsToAdd = [];
+    newAdmins.forEach(admin => {
+        if (existingPlayers.includes(admin) && !existingAdmins.includes(admin)) {
+            adminsToAdd.push(admin);
+        }
+    });
+
+    // ADD PLAYERS TO GROUP
+    try {
+        for (const newAdmin of adminsToAdd) {
+            let admin = await User.findById(newAdmin);
+            existingGroup.admins.push(admin);
+            await existingGroup.save();
+        }
+    } catch (e) {
+        return next(new HttpError("Adding admins failed, please try again", 500));
+    }
+
+    res.status(201).json({message: "Success"});
+};
 
 exports.createGroup = createGroup;
 exports.addPlayerToGroup = addPlayerToGroup;
+exports.addAdminToGroup = addAdminToGroup;
--- routes/group-routes.js
+++ routes/group-routes.js
@@ -16,6 +16,12 @@ router.post("/addplayer", [
     check('user').not().isEmpty()
 ], groupController.addPlayerToGroup);
 
+router.post("/addadmin", [
+    check('newAdmins').isArray({min: 1}),
+    check('group').not().isEmpty(),
+    check('user').not().isEmpty()
+], groupController.addAdminToGroup);
+
 // router.post("/login", [
 //     check('username').isLength({min: 5}),
 //     check('password').isLength({min: 6})
